6e730089349960631fd3443117ae6fda549918c7
bonelli95 <rodrigobonelli95@gmail.com>
2024-04-08 16:41:54 +0100

    trusted origins *

--- setup/settings.py
+++ setup/settings.py
@@ -62,10 +62,7 @@ ALLOWED_HOSTS = ['my-training-9fdf0c00a234.herokuapp.com',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://my-training-9fdf0c00a234.herokuapp.com/',
-    'https://my-training-9fdf0c00a234.herokuapp.com/login',
-    'https://my-training-9fdf0c00a234.herokuapp.com/register',
-    'https://my-training-9fdf0c00a234.herokuapp.com/exercise_details',
+    'https://*.herokuapp.com/',
 ]
 
 CSRF_COOKIE_SECURE=True

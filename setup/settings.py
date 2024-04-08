b9ea2f069c43be946dc96c36db61fe948f23a490
bonelli95 <rodrigobonelli95@gmail.com>
2024-04-08 17:39:39 +0100

    -1 url

--- setup/settings.py
+++ setup/settings.py
@@ -63,7 +63,6 @@ ALLOWED_HOSTS = ['my-training-9fdf0c00a234.herokuapp.com',
 
 CSRF_TRUSTED_ORIGINS = [
     'https://my-training-9fdf0c00a234.herokuapp.com/',
-    'https://my-training-9fdf0c00a234.herokuapp.com',
     'https://my-training-9fdf0c00a234.herokuapp.com/login',
     'https://my-training-9fdf0c00a234.herokuapp.com/register',
     'https://my-training-9fdf0c00a234.herokuapp.com/exercise_details',

711dafddbf406f8b6c923db02593418dca66dee2
bonelli95 <rodrigobonelli95@gmail.com>
2024-04-09 10:06:07 +0100

    f'string database config

--- setup/settings.py
+++ setup/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + BASE_DIR / 'db.sqlite3')
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
 }
 
 

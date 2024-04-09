5247b16e8f2211b7b77c6108bb9f6f9b5b7eab98
bonelli95 <rodrigobonelli95@gmail.com>
2024-04-09 10:48:48 +0100

    database

--- setup/settings.py
+++ setup/settings.py
@@ -96,9 +96,11 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
